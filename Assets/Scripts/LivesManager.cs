5056354e786caf70d24ca2a2e3b7fac537ab05c8
Marcopolo Agostadero <marcopolo.agostadero@my.utsa.edu>
2026-03-28 13:59:31 -0500

    Update LivesManager.cs

--- Assets/Scripts/LivesManager.cs
+++ Assets/Scripts/LivesManager.cs
@@ -1,16 +1,16 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LivesManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public static int playerHP = 3;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(playerHP <= 0)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
     }
 }
