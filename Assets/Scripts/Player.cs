3a1fa93f2efe81627ccf1363b4a5c284d4e66e25
Marcopolo Agostadero <marcopolo.agostadero@my.utsa.edu>
2026-03-28 15:29:28 -0500

    create HeadManager.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,4 +4,8 @@ using UnityEngine.SceneManagement;
 public class Player : MonoBehaviour
 {
     public int playerHP = 3;
+
+    public float cooldownTime = 10;
+    public float timer = 0;
+    public bool isCooldown = false;
 }
