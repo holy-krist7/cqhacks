613224b151c9b62ef4d742e8227e9f504f1f0cc0
Marcopolo Agostadero <marcopolo.agostadero@my.utsa.edu>
2026-03-28 12:12:51 -0500

    Update PauseMenController.cs

--- Assets/Scripts/PauseMenuController.cs
+++ Assets/Scripts/PauseMenuController.cs
@@ -1,16 +1,15 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenuController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void OnResumeClick()
     {
-        
+        // resume game
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnExitClick()
     {
-        
+        SceneManager.LoadScene("MainMenu");
     }
 }
