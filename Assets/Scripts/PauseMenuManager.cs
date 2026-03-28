d51570a9e8e6e11c03bc716ac50437bff71dc597
Marcopolo Agostadero <marcopolo.agostadero@my.utsa.edu>
2026-03-28 12:30:26 -0500

    Update PauseMenuManager.cs

--- Assets/Scripts/PauseMenuManager.cs
+++ Assets/Scripts/PauseMenuManager.cs
@@ -1,16 +1,52 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenuManager : MonoBehaviour
 {
+    public GameObject pauseMenu;
+    public static bool isPaused;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        pauseMenu.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnCancel()
+    {
+        if (isPaused)
+        {
+            ResumeGame();
+        }
+        else
+        {
+            PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
     }
 }
