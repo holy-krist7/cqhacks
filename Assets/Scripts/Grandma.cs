39f8abb2f0af2969480923a23c99f7bec1b565bd
Marcopolo Agostadero <marcopolo.agostadero@my.utsa.edu>
2026-03-28 11:42:21 -0500

    Create GrandmaManager.cs

--- Assets/Scripts/Grandma.cs
+++ Assets/Scripts/Grandma.cs
@@ -2,15 +2,50 @@ using UnityEngine;
 
 public class Grandma : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float xMoveSpeed = 5;
+    public float yMoveSpeed = 5;
+    public float deadZone = -12;
+
+    private float xDirection = -1f;
+    private SpriteRenderer sr;
+
+    private void Awake()
     {
-        
+        sr = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
+    public void SetDirection(float direction)
+    {
+        xDirection = direction;
+
+        if (sr != null)
+        {
+            sr.flipX = direction < 0 ? false : true;
+        }
+    }
+
+    private void Start()
+    {
+        xMoveSpeed = Random.Range(2, 4);
+    }
+
     void Update()
     {
-        
+        Vector3 movement = new Vector3(xDirection * xMoveSpeed, -yMoveSpeed, 0f);
+        transform.position += movement * Time.deltaTime;
+
+        if (transform.position.y < deadZone)
+        {
+            Debug.Log("Grandma Deleted");
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            Debug.Log("Grandma hit the player");
+        }
     }
 }
