45758807e86d2b37501ac00ed1aa2f9576deba46
Marcopolo Agostadero <marcopolo.agostadero@my.utsa.edu>
2026-04-11 12:36:33 -0500

    Fix the cooldown system

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -3,6 +3,7 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using System;
 
 public class Obstacle : MonoBehaviour
 {
@@ -18,56 +19,28 @@ public class Obstacle : MonoBehaviour
         player = car.GetComponent<Player>();
     }
 
-    private void Update()
-    {
-        if (player.isCooldown == true)
-        {
-            player.timer += Time.deltaTime;
-
-            if (player.timer >= player.cooldownTime)
-            {
-                player.isCooldown = false;
-                Debug.Log("Player is out of cooldown");
-            }
-        }
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(player.isCooldown == false)
         {
-            if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+            if (collision.CompareTag("Player"))
             {
                 Debug.Log("obstacle hit the player");
 
                 player.playerHP--;
+
                 SpriteShake head = GameObject.FindGameObjectWithTag("HeadSprite").GetComponent<SpriteShake>();
                 head.Shake(.2f, 60);
 
                 player.isCooldown = true;
+                player.StartFlash();
                 Debug.Log("Player is in cooldown");
 
                 if (player.playerHP <= 0)
                 {
-                    //Time.timeScale = 0f;
-                   // StartCoroutine(TimerRoutine(1f));
-                    if (isTimerRunning == false)
-                    {
-                        SceneManager.LoadScene("GameOver");
-                    }
+                    SceneManager.LoadScene("GameOver");
                 }
             }
         }
-    }
-
-    IEnumerator TimerRoutine(float duration)
-    {
-        isTimerRunning = true;
-        Debug.Log("Timer Started");
-
-        yield return new WaitForSeconds(duration);
-
-        Debug.Log("Timer Finished!");
-        isTimerRunning = false;
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,7 +6,48 @@ public class Player : MonoBehaviour
 {
     public int playerHP = 3;
 
-    public float cooldownTime = 10;
+    public float cooldownTime = 2f;
     public float timer = 0;
     public bool isCooldown = false;
+
+    private SpriteRenderer sr;
+
+    private void Start()
+    {
+        sr = GetComponentInChildren<SpriteRenderer>();   
+    }
+
+    private void Update()
+    {
+        if (isCooldown)
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= cooldownTime)
+            {
+                isCooldown = false;
+                timer = 0f;
+                Debug.Log("Player is out of cooldown");
+            }
+        }
+    }
+
+    public IEnumerator FlashSprite()
+    {
+        while (isCooldown)
+        {
+            sr.enabled = false;
+            yield return new WaitForSeconds(0.1f);
+
+            sr.enabled = true;
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        sr.enabled = true;
+    }
+
+    public void StartFlash()
+    {
+        StartCoroutine(FlashSprite());
+    }
 }
