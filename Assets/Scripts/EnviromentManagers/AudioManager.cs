8de9aab84201c9d04cf12716cdfb868ae3c9ecc8
Marcopolo Agostadero <marcopolo.agostadero@my.utsa.edu>
2026-05-21 12:15:10 -0500

    Add logic to sounds and the audio manager

--- Assets/Scripts/EnviromentManagers/AudioManager.cs
+++ Assets/Scripts/EnviromentManagers/AudioManager.cs
@@ -1,17 +1,31 @@
 using UnityEngine;
 using UnityEngine.Audio;
+using System;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Sound[] sounds;
+    
+    void Awake()
     {
-        
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Play(string name)
     {
-        
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            return;
+        }
+        s.source.Play();
     }
 }
--- Assets/Scripts/Sound.cs
+++ Assets/Scripts/Sound.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
-public class Sound : MonoBehaviour
+[System.Serializable]
+public class Sound
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public string name;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+
+    [Range(.1f, 3f)]
+    public float pitch;
+
+    public bool loop;
+
+    [HideInInspector]
+    public AudioSource source;
 }
